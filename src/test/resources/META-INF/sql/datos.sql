696fbc008c54e31b276c1151649e21d080ae4dca
Sara García <gh19009@ues.edu.sv>
2022-03-24 10:55:00 -0600

    Pruebas de integracion

---
+++ src/test/resources/META-INF/sql/datos.sql
@@ -0,0 +1,2 @@
+INSERT INTO estado (nombre, fecha_creacion, observaciones) VALUES ("nuevo",'2022-03-07',"ninguna");
+INSERT INTO estado (nombre, fecha_creacion, observaciones) VALUES ("en proceso",'2022-03-06',"ninguna");
