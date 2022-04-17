eb6edd62e08bbd8a7f0d6276346c6c9d10deca27
Sara García <gh19009@ues.edu.sv>
2022-04-17 15:34:22 -0600

    Api para tabla Estado

--- src/main/java/ues/pruebacrud/resources/EstadoResource.java
+++ src/main/java/ues/pruebacrud/resources/EstadoResource.java
@@ -9,8 +9,13 @@ import java.io.Serializable;
 import java.util.List;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import ues.pruebacrud.controller.ControllerEstado;
@@ -35,4 +40,44 @@ public class EstadoResource implements Serializable{
         return Response.ok(registros).header("Total-registros", total).build();
     }
     
+    @GET
+    @Path("/{idEstado}")
+    @Produces({"application/json; charset=UTF-8"})
+    public Estado findById(@PathParam("idEstado")int id){
+        Estado registro = toBean.findById(id);
+        return registro;
+    }
+    
+    @GET
+    @Produces({"application/json; charset=UTF-8"})
+    @Path("/contar")
+    public Response contar(){
+        Long resultado = toBean.contar();
+        return Response.ok(resultado).build();
+    }
+    
+    @POST
+    @Consumes({"application/json; charset=UTF-8"})
+    @Produces({"application/json; charset=UTF-8"})
+    public Response crear(Estado estado){
+        toBean.crear(estado);
+        Estado registro = toBean.findById(estado.getIdEstado());
+        return Response.ok(registro).build();
+    }
+    
+    @PUT
+    @Consumes({"application/json; charset=UTF-8"})
+    @Produces({"application/json; charset=UTF-8"})
+    public Response modificar(Estado estado){
+        toBean.modificar(estado);
+        Estado registro = toBean.findById(estado.getIdEstado());
+        return Response.ok(registro).build();
+    }
+    
+    @DELETE
+    @Path("/{idEstado}")
+    public void eliminar(@PathParam("idEstado")int id){
+        Estado registro = toBean.findById(id);
+        toBean.eliminar(registro);
+    }
 }
