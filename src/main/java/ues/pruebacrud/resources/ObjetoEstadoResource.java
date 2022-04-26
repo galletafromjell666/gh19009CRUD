d5bc5d91d8e1487066b27d2af82ab1398bd186dd
Sara García <gh19009@ues.edu.sv>
2022-04-26 16:35:10 -0600

    modificacion

--- src/main/java/ues/pruebacrud/resources/ObjetoEstadoResource.java
+++ src/main/java/ues/pruebacrud/resources/ObjetoEstadoResource.java
@@ -7,16 +7,19 @@ package ues.pruebacrud.resources;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import ues.pruebacrud.controller.ControllerObjetoEstado;
 import ues.pruebacrud.entities.ObjetoEstado;
@@ -50,10 +53,22 @@ public class ObjetoEstadoResource implements Serializable{
     
     @GET
     @Produces({"application/json; charset=UTF-8"})
-    @Path("/contar")
-    public Response contar(){
-        Long resultado = toBean.contar();
-        return Response.ok(resultado).build();
+    public Response findRange(
+            @QueryParam(value="first")
+            @DefaultValue(value="0") int first,
+            @QueryParam(value="pageSize")
+            @DefaultValue(value="10") int pageSize){
+        List<ObjetoEstado> registros = toBean.findRange(first, pageSize);
+        Long total = toBean.contar();
+        return Response.ok(registros)
+                .header("Total-registros", total)
+                .build();
+    }
+    
+    @GET
+    @Path("contar")
+    public CompletableFuture<Long> contar(){
+        return CompletableFuture.supplyAsync(toBean::contar);
     }
     
     @POST
